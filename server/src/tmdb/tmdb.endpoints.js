c3df89130ac744ba63dd5e1284b22c0c8d07fc36
Thithi2103 <hoangthiedklm@gmail.com>
2023-02-28 21:44:03 +0700

    new commit

---
+++ server/src/tmdb/tmdb.endpoints.js
@@ -0,0 +1,36 @@
+import tmdbConfig from "./tmdb.config.js";
+
+const tmdbEndpoints = {
+    mediaList: ({mediaType, mediaCategory, page}) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaCategory}`, {page}
+    ),
+    mediaDetail: ({mediaType, mediaId}) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}`
+    ),
+    mediaGenres: ({mediaType}) => tmdbConfig.getUrl(
+        `genre/${mediaType}/List`
+    ),
+    mediaCredits: ({mediaType, mediaId}) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/credits`
+    ),
+    mediaVideos: ({mediaType, mediaId}) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/videos`
+    ),
+    mediaRecommend: ({mediaType, mediaId}) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/recommendation`
+    ),
+    mediaImages: ({mediaType, mediaId}) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/images`
+    ),
+    mediaSearch: ({mediaType, query, page}) => tmdbConfig.getUrl(
+        `search/${mediaType}`, {query, page}
+    ),
+    personDetail: ({personId}) => tmdbConfig.getUrl(
+        `person/${personId}`
+    ),
+    personMedias: ({personId}) => tmdbConfig.getUrl(
+        `person/${personId}/combimed_credits`
+    )
+};
+
+export default tmdbEndpoints;
